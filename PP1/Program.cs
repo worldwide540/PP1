d1bcb28777e2fee8285c8eaa9330d09639020237
narek <narek@DESKTOP-98JS365>
2024-06-20 16:57:20 +0300

    Добавьте файлы проекта.

---
+++ PP1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class TemperatureConverter
+{
+    static double CelsiusToFahrenheit(double celsius)
+    {
+        return (celsius * 9 / 5) + 32;
+    }
+
+    static double FahrenheitToCelsius(double fahrenheit)
+    {
+        return (fahrenheit - 32) * 5 / 9;
+    }
+
+    static void Main()
+    {
+        double celsiusTemperature = 25.0; // Температура в Цельсиях
+        double fahrenheitTemperature = CelsiusToFahrenheit(celsiusTemperature);
+        Console.WriteLine($"{celsiusTemperature} градусов Цельсия = {fahrenheitTemperature} градусов Фаренгейта");
+
+        double newCelsiusTemperature = FahrenheitToCelsius(fahrenheitTemperature);
+        Console.WriteLine($"{fahrenheitTemperature} градусов Фаренгейта = {newCelsiusTemperature} градусов Цельсия");
+    }
+}
